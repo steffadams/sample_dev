9fc5c05631ff4cf91b7d726e0f4aa5c3e9a10613
steffadams <steffadams.sa@gmail.com>
2014-09-02 15:37:20 +0100

    Provisioned composer install

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,8 @@
+# ENSURE LATEST UPDATES APPLIED
 exec { "apt-get update":
   path => "/usr/bin",
 }
+# INSTALLL APACHE WEB SERVICE AND ENSURE RUNNING
 package { "apache2":
   ensure  => present,
   require => Exec["apt-get update"],
@@ -9,10 +11,31 @@ service { "apache2":
   ensure  => "running",
   require => Package["apache2"],
 }
+
+# SET SITE LOCATION
 file { "/var/www/sample-webapp":
   ensure  => "link",
   target  => "/vagrant/sample-webapp",
   require => Package["apache2"],
   notify  => Service["apache2"],
 }
-package { "vim": ensure => present, }
+
+# REQUIRED PACKAGES
+package { "python-software-properties": ensure => present, }
+package { ["vim","vim-addon-manager", "vim-puppet"]: ensure => present, }
+package { ["php5","php5-cli","php5-mcrypt"]: ensure => present, }
+package { ["mysql-server","php5-mysql"]: ensure => present, }
+package { ["unzip","curl","openssl"]: ensure => present, }
+
+# DOWNLOAD COMPOSER
+exec { "get_composer":
+    command => "/usr/bin/curl -sS https://getcomposer.org/installer | php",
+    unless  => "/usr/bin/test -f /usr/local/bin/composer",
+    require => Package["php5-cli"], 
+}
+
+# INSTALL COMPOSER
+exec {"install_composer":
+    command => "/bin/mv /home/vagrant/composer.phar /usr/local/bin/composer",
+    onlyif  => "/usr/bin/test -f /home/vagrant/composer.phar",
+}
